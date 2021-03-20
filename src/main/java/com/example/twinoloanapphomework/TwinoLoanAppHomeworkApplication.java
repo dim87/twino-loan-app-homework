29d9d6aebfa3cdf3efdbf7dc7e2b94f0a5dabd72
dls860 <dmitrijs.latnikovs@havi.com>
2021-03-20 08:38:08 +0200

    Initial commit

---
+++ src/main/java/com/example/twinoloanapphomework/TwinoLoanAppHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.twinoloanapphomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinoLoanAppHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwinoLoanAppHomeworkApplication.class, args);
+	}
+
+}
